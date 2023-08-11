0a94eccf59661ba30085b38b73bc384510d36102
devendra <98072876+devendrakanojiya@users.noreply.github.com>
2023-08-11 02:36:09 +0000

    initial commit

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html", title="Admin")
+
+if __name__ == '__main__':
+    app.run()
